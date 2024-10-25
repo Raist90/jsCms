48b0519fb8ed9cc35c25b05bd6137a4824eb146c
Manuel De Toma <manuel.detoma@gmail.com>
2024-10-25 17:47:08 +0200

    feat: add prettier and update eslint conf

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-export default withNuxt({
+export default withNuxt(eslintPluginPrettierRecommended, {
   rules: {
+    "vue/html-self-closing": "off",
     "vue/multi-word-component-names": "off",
     "@typescript-eslint/no-explicit-any": "off",
   },
