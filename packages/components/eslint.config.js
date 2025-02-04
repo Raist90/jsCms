b205f90f33c5734227ace24e018dc1fc84e0c641
Manuel De Toma <manuel.detoma@gmail.com>
2025-02-04 22:48:02 +0100

    feat: add some eslint rules on components package

--- packages/components/eslint.config.js
+++ packages/components/eslint.config.js
@@ -15,5 +15,11 @@ export default [
   {
     files: ["**/*.vue"],
     languageOptions: { parserOptions: { parser: tseslint.parser } },
+    rules: {
+      "vue/attribute-hyphenation": ["error", "never"],
+      "vue/html-self-closing": "off",
+      "vue/multi-word-component-names": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
   },
 ]
