beca8b24a3b722b124d89b02c617ba7188d9c5ca
Manuel De Toma <manuel.detoma@gmail.com>
2025-03-29 21:36:53 +0100

    chore: remove ClientOnly everywhere

--- apps/nuxt/components/DocumentsList.vue
+++ apps/nuxt/components/DocumentsList.vue
@@ -36,21 +36,19 @@ function getFallbackTitle(document: DocumentEntry) {
       <h2 class="font-bold">{{ capitalize(documentType) }} list</h2>
     </header>
 
-    <ClientOnly>
-      <nav v-if="documentsList.length" class="p-4">
-        <ul class="grid gap-y-2">
-          <li v-for="document in documentsList" :key="document.id">
-            <NuxtLink
-              :class="['block p-2 rounded-md']"
-              :to="`/documents/${document.type}/id/${document.id}`"
-              :active="currentPath.includes(document.id)"
-              activeClass="bg-blue-500"
-            >
-              {{ document.data.title || getFallbackTitle(document) }}
-            </NuxtLink>
-          </li>
-        </ul>
-      </nav>
-    </ClientOnly>
+    <nav v-if="documentsList.length" class="p-4">
+      <ul class="grid gap-y-2">
+        <li v-for="document in documentsList" :key="document.id">
+          <NuxtLink
+            :class="['block p-2 rounded-md']"
+            :to="`/documents/${document.type}/id/${document.id}`"
+            :active="currentPath.includes(document.id)"
+            activeClass="bg-blue-500"
+          >
+            {{ document.data.title || getFallbackTitle(document) }}
+          </NuxtLink>
+        </li>
+      </ul>
+    </nav>
   </section>
 </template>
--- apps/nuxt/pages/documents/[name]/index.vue
+++ apps/nuxt/pages/documents/[name]/index.vue
@@ -1,41 +1,27 @@
 <script setup lang="ts">
-import { useDocumentsStore } from "~/store/documentsStore";
-
 const route = useRoute();
 const { currentDocumentEntryName: documentEntryName } =
   useExtractRouteData(route);
-
-const documentsStore = useDocumentsStore();
-const { documentsEntries } = storeToRefs(documentsStore);
-const { filterDocumentsEntriesByType } =
-  useFilterDocumentsEntries(documentsEntries);
-const documentsList = filterDocumentsEntriesByType(documentEntryName);
 </script>
 
 <template>
-  <ClientOnly>
-    <section class="w-full">
-      <header
-        class="p-4 border-b border-gray-700 flex justify-between items-center h-16"
-      >
-        <div class="flex gap-x-3 items-center">
-          <h2
-            class="font-bold"
-            v-text="`${capitalize(documentEntryName)} list`"
-          />
-        </div>
-
-        <UIButton
-          variant="outline"
-          size="sm"
-          @click="navigateTo(`/documents/${documentEntryName}/add`)"
-          >Add a new {{ documentEntryName }}</UIButton
-        >
-      </header>
-
-      <div v-if="documentsList.length" class="p-4">
-        <!-- <UTable v-model="selected" :columns="columns" :rows="list" /> -->
+  <section class="w-full">
+    <header
+      class="p-4 border-b border-gray-700 flex justify-between items-center h-16"
+    >
+      <div class="flex gap-x-3 items-center">
+        <h2
+          class="font-bold"
+          v-text="`${capitalize(documentEntryName)} list`"
+        />
       </div>
-    </section>
-  </ClientOnly>
+
+      <UIButton
+        variant="outline"
+        size="sm"
+        @click="navigateTo(`/documents/${documentEntryName}/add`)"
+        >Add a new {{ documentEntryName }}</UIButton
+      >
+    </header>
+  </section>
 </template>
