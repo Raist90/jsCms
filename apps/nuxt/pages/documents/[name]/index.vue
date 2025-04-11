51470772a272a8c648d8d98bebf020b1a5c8bbac
Manuel De Toma <manuel.detoma@gmail.com>
2025-04-11 21:57:55 +0200

    feat: add Table component

--- apps/nuxt/pages/documents/[name]/index.vue
+++ apps/nuxt/pages/documents/[name]/index.vue
@@ -1,7 +1,30 @@
 <script setup lang="ts">
+import { useDocumentsStore } from "~/store/documentsStore";
+
 const route = useRoute();
 const { currentDocumentEntryName: documentEntryName } =
   useExtractRouteData(route);
+
+const { documentsEntries: allDocumentsEntries } = useDocumentsStore();
+const documentsEntries = computed(() => {
+  return allDocumentsEntries.filter(
+    (entry) => entry.type === documentEntryName.value,
+  );
+});
+
+const columns = [
+  "id",
+  ...Object.keys(documentsEntries.value[0].data),
+  "createdAt",
+];
+
+const rows = computed(() => {
+  return documentsEntries.value.map((entry) => ({
+    id: entry.id,
+    ...entry.data,
+    timestamp: entry.timestamp,
+  }));
+});
 </script>
 
 <template>
@@ -23,5 +46,7 @@ const { currentDocumentEntryName: documentEntryName } =
         >Add a new {{ documentEntryName }}</UIButton
       >
     </header>
+
+    <UITable :columns :rows />
   </section>
 </template>
---
+++ packages/ui/src/components/Table.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+type Props = {
+  columns: string[];
+  rows: Record<string, string>[];
+};
+defineProps<Props>();
+</script>
+
+<template>
+  <section class="p-4 flex flex-col gap-y-4 overflow-x-auto">
+    <table class="table-auto border border-gray-700 border-collapse text-sm">
+      <thead class="text-left">
+        <tr>
+          <th
+            v-for="column in columns"
+            :key="column"
+            class="p-2 border border-gray-700"
+            v-text="column"
+          />
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="row in rows" :key="row.id">
+          <td
+            v-for="(val, index) in row"
+            :key="index"
+            class="p-2 border border-gray-700"
+            v-text="val"
+          />
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
