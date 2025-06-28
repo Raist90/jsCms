9c6a64d975a2ad4db3fd05440f7b021f2f082dc0
Manuel De Toma <manuel.detoma@gmail.com>
2025-06-28 16:44:58 +0200

    chore: refresh bun.lockb and drizzle meta

--- apps/nuxt/drizzle/0000_sharp_blink.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `api` (
-	`id` text PRIMARY KEY NOT NULL,
-	`type` text NOT NULL,
-	`data` text NOT NULL,
-	`definition` text NOT NULL,
-	`timestamp` text DEFAULT (current_timestamp) NOT NULL
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX `api_id_unique` ON `api` (`id`);
